ba900e7709c651a4009b52e4f94c30df88c03daa
unknown <kikopesqueira96@gmail.com>
2020-12-08 14:41:30 -0300

    exercicios do dia 08-12-2020

---
+++ java/aulas/src/exercicio2/tarefa3.java
@@ -0,0 +1,26 @@
+package exercicio2;
+
+import  java.util.Scanner ;
+
+public class tarefa3 {
+	//exercico 8 do dia 8/12/2020
+	public static void main(String[] args)
+	{
+		Scanner ler = new Scanner (System.in);
+		int N;
+		
+		System.out.printf("Entre com um número inteiro: ");
+		N = ler.nextInt();
+		
+		if (N>100)
+		{
+			System.out.printf("resposta:" + N + ".");
+			
+		}
+		else
+		{
+			N =0;
+			System.out.printf("resposta:0" + ".");
+		}
+	}
+}
